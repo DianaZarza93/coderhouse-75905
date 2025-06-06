a38738dd30682d6aa69ee017ff1ce19bc159b2a6
Diana Zarza <dianazarza088@gmail.com>
2025-06-06 19:28:31 -0300

    agrego la aplicación personalizada 'core' y defino varias vistas para saludar

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def saludar(request):
+    return HttpResponse("¡Hola desde Django!")
+
+def saludar_con_etiqueta(request):
+    return HttpResponse('<h1 style="color:red">HOLA</h1>')
+
+def saludar_con_parametros(request, nombre: str, apellido: str):
+    nombre = nombre.capitalize()
+    apellido = apellido.upper()
+    return HttpResponse(f"{apellido}, {nombre}")
