44ef592c3a9b3e18032ba72553f0c8136db371e9
Diana Zarza <dianazarza088@gmail.com>
2025-06-09 11:51:58 -0300

    agrego la vista 'tirar_dado' y las plantillas HTML correspondientes para mostrar el resultado del tiro de un dado

--- core/views.py
+++ core/views.py
@@ -19,3 +19,23 @@ def saludar_con_parametros(request, nombre: str, apellido: str):
     nombre = nombre.capitalize()
     apellido = apellido.upper()
     return HttpResponse(f"{apellido}, {nombre}")
+
+
+
+def tirar_dado(request):
+    from datetime import datetime
+    from random import randint
+
+    tiro_de_dado = randint(1, 6)
+
+    if tiro_de_dado == 6:
+        mensaje = f"Has tirado el dado y has sacado {tiro_de_dado}!. ¡Ganaste!"
+    else:   
+        mensaje = f"Has tirado el dado y has sacado {tiro_de_dado}!. ¡Sigue intentando!"
+
+    datos = {
+        "titulo": "Tiro de Dados",
+        "mensaje": mensaje,
+        "fecha": datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+    }
+    return render(request, 'core/dados.html', context=datos)
